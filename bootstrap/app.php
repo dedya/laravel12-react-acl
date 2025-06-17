f7d676dd9d64ea27ea99042e251297c3d0be2f90
Dedy Adhiewirawan <dedy.adhiewirawan@gmail.com>
2025-06-17 10:39:19 +0700

    add inertia

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        //added after installation of inertia
          $middleware->alias([
             'inertia' => HandleInertiaRequests::class,
         ]);
