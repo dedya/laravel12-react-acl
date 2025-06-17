25c1da86f51a779680fc22b514c828e12152efb2
Suhendar <hendarsyahss@gmail.com>
2025-06-17 22:06:56 +0700

    update tailwindcss 4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // Import the Tailwind CSS Vite plugin
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
