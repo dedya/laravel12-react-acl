5150f8dfb529f342e1636dbd2b19985117157a66
Dedy Adhiewirawan <dedy.adhiewirawan@gmail.com>
2025-06-22 16:53:32 +0700

    add blameable to track created_by, updated_by, deleted_by to user groups

--- app/Models/UserGroup.php
+++ app/Models/UserGroup.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\LogsModelActivity;
+use App\Traits\Blameable;
 
 class UserGroup extends Model
 {
     use LogsModelActivity;
-
+    use Blameable;
+    
     protected $fillable = ['name'];
 
     public function users()
